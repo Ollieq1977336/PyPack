54d42de7a78ac7af2405042a3e9ec9380480243e
Ollieq1977336 <olliequayle08@icloud.com>
2024-07-23 07:58:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyPack',
+    version='0.1',
+    packages=find_packages(),
+    description='A collection of Tools built for Python',
+    author='Ollie Quayle',
+    python_requires='>=3.6',  # Specify the minimum Python version required
+)
