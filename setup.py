985d06f412acf78c72e1b6c56cd73c91d91aa27f
Ollieq1977336 <olliequayle08@icloud.com>
2024-07-23 09:53:47 +0100

    Update Setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PyPack',
-    version='0.1',
+    name="pypack",
+    version="0.1.0",
     packages=find_packages(),
-    description='A collection of Tools built for Python',
-    author='Ollie Quayle',
-    python_requires='>=3.6',  # Specify the minimum Python version required
+    install_requires=[
+        # List other dependencies here
+    ],
+    description="A collection of tools to simplify your Python experience!",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Ollie Quayle",
+    author_email="your.email@example.com",
+    license="MIT",
+    url="https://github.com/Ollieq1977336/PyPack",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
