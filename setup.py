61b5e2c48fec30affcb1b782d9db5761362a63f5
Ollieq1977336 <olliequayle08@icloud.com>
2024-07-23 10:07:20 +0100

    Signed-off-by: Ollieq1977336 <olliequayle08@icloud.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pypack",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        # List other dependencies here
-    ],
-    description="A collection of tools to simplify your Python experience!",
+    name='pypack',
+    version='0.1.0',
+    description='A collection of tools to simplify your Python experience!',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author="Ollie Quayle",
-    author_email="your.email@example.com",
-    license="MIT",
-    url="https://github.com/Ollieq1977336/PyPack",
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/Ollieq1977336/PyPack',
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[],  # List your dependencies here
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.6',
 )
